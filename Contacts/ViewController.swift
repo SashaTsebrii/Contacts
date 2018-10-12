b2a115100c39529777dc30f3614637d8d4044719
SashaTsebrii <sashatsebrii@gmail.com>
2018-10-12 13:53:24 +0300

    Initial Commit

---
+++ Contacts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Contacts
+//
+//  Created by Aleksandr Tsebrii on 10/12/18.
+//  Copyright © 2018 Aleksandr Tsebrii. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
